98d752558daa21ff060d50ee74fcb90f94933136
fomka94 <fom-aina@mail.ru>
2022-06-06 23:45:48 +0900

    Initial Commit

---
+++ hw12-pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw12-pomodoro
+//
+//  Created by Фомина Айина Валерьевна on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hw12-pomodoroTests/hw12_pomodoroTests.swift
@@ -0,0 +1,33 @@
+//
+//  hw12_pomodoroTests.swift
+//  hw12-pomodoroTests
+//
+//  Created by Фомина Айина Валерьевна on 06.06.2022.
+//
+
+import XCTest
+@testable import hw12_pomodoro
+
+class hw12_pomodoroTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
